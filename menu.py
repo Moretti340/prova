ed4c819326d789b80783427615d7bcc5d292875a
Moretti340 <moretti.andrea.08@itisdalmine.edu.it>
2024-12-17 09:57:24 +0100

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,19 @@
+while True:
+    print("benvenuto nella nostra calcolatrice")
+    print("inserisci la operazione che vuoi effettuare ")
+    scelta = int(input("1 sottrazione 2 addizione "))
+    if scelta==0:
+        break
+    elif scelta==1:
+        n1=int(input("inserisci il primo numero "))
+        n2=int(input("inserisci il secondo numero "))
+        diff=n1-n2
+        print("la differenza e ",diff)
+    
+    elif scelta==2:
+        n1=int(input("inserisci il primo numero "))
+        n2=int(input("inserisci il secondo numero "))
+        somma=n1+n2
+        print("la somma e ",somma)
+    else:
+        print("Scelta non corretta!")
